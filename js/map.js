c25e56e0205b5c4abc438ef20c69f12bc31d62f0
GraphicRabbit <joshuakrull@gmail.com>
2021-09-25 18:41:53 -0500

    Added long/lat to Google map for contact form

--- js/map.js
+++ js/map.js
@@ -87,9 +87,9 @@ var styleArray = [
 
 // Create a map object and specify the DOM element for display.
 // The latitude and longitude to center the map (always required)
-var latlng = new google.maps.LatLng(40.7143528, -74.0059731);
+var latlng = new google.maps.LatLng(42.6641, -93.9044);
 var map = new google.maps.Map(document.getElementById("map"), {
-  center: latlng, 
+  center: latlng,
   scrollwheel: false,
 // Apply the map style array to the map.
 // How zoomed in you want the map to start at (always required)
